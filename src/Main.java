db4fab71d681759fbf0063f41344e3c331c66a28
Chí Hiển <89452066+ALX-Hien2k2@users.noreply.github.com>
2023-01-06 00:27:39 +0700

    Setup base

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author chihi
+ */
+public class Main {
+    public static void main(String[] agrs) {
+        System.out.println("Hello to this Java Project");
+    }
+}
